03db7dd0f5d068acf15ef95f235abc8da8b9f3d7
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-04-27 02:07:45 +0300

    Added some queues and stacks exercises

---
+++ 01.StacksAndQueues/01.ReverseStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string input = Console.ReadLine();
+            Stack<Char> stack = new Stack<char>(input);
+            while(stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
---
+++ 01.StacksAndQueues/02.StackSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack=new Stack<int>(input);
+            string command;
+            while((command = Console.ReadLine()).ToLower() != "end")
+            {
+                string [] inpuutData=command.Split();
+                if (inpuutData[0].ToLower() == "add")
+                {
+                    stack.Push(int.Parse(inpuutData[1]));
+                    stack.Push(int.Parse(inpuutData[2]));
+                }
+                else if (inpuutData[0].ToLower() == "remove")
+                {
+                    int count=int.Parse(inpuutData[1]);
+                    if (count > stack.Count)
+                    {
+                        continue;
+                    }
+                    while (count > 0)
+                    {
+                        stack.Pop();
+                        count--;
+                    }
+                }
+            }
+            int sum = stack.Sum();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
