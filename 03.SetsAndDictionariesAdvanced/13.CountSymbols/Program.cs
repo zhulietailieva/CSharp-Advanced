030c17063edd761e8c8c4dbaff3df3f1ecfe9809
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-05-03 13:10:11 +0300

    Added some new Dictionary and SortedDictionary exercises.

---
+++ 03.SetsAndDictionariesAdvanced/13.CountSymbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _13.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input=Console.ReadLine();
+          SortedDictionary<char,int> chars=new SortedDictionary<char,int>();
+         for(int i = 0; i < input.Length; i++)
+            {
+                if (!chars.ContainsKey(input[i]))
+                {
+                    chars.Add(input[i],1);
+                }
+                else
+                {
+                    chars[input[i]]++;
+                }
+            }
+            foreach (var ch in chars)
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+            }
+        }
+    }
+}
---
+++ 03.SetsAndDictionariesAdvanced/14.Wardrobe/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14.Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n=int.Parse(Console.ReadLine());
+            Dictionary<string,Dictionary<string,int>> dic=new Dictionary<string,Dictionary<string,int>>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputData=Console.ReadLine().Split(" -> ",StringSplitOptions.RemoveEmptyEntries);
+                string color=inputData[0];
+                //if there is NO such color in the wardrobe
+                if (!dic.ContainsKey(color))
+                {
+                    dic.Add(color, new Dictionary<string,int>());
+                    //if the clothes are more than 1
+                    if (inputData[1].Contains(','))
+                    {
+                        string[] clothes = inputData[1].Split(",");
+                        //check if the cloth is already on the inside dic
+                        foreach (string cloth in clothes)
+                        {
+                            //if there is no such cloth
+                            if (!dic[color].ContainsKey(cloth))
+                            {
+                                dic[color].Add(cloth, 1);
+                            }
+                            else//increase the count
+                            {
+                                dic[color][cloth]++;
+                            }
+                        }
+                    }
+                    //if there is only one cloth
+                    else
+                    {
+                        string cloth=inputData[1];
+                        if (!dic[color].ContainsKey(cloth))
+                        {
+                            dic[color].Add(cloth, 1);
+                        }
+                        else//increase the count
+                        {
+                            dic[color][cloth]++;
+                        }
+                    }
+                    
+                }
+                //if that color is already in the wardrobe
+                else
+                {
+                    if (inputData[1].Contains(','))
+                    {
+                        string[] clothes = inputData[1].Split(",");
+                        //check if the cloth is already on the inside dic
+                        foreach (string cloth in clothes)
+                        {
+                            //if there is no such cloth
+                            if (!dic[color].ContainsKey(cloth))
+                            {
+                                dic[color].Add(cloth, 1);
+                            }
+                            else//increase the count
+                            {
+                                dic[color][cloth]++;
+                            }
+                        }
+                    }
+                    //if there is only one cloth
+                    else
+                    {
+                        string cloth = inputData[1];
+                        if (!dic[color].ContainsKey(cloth))
+                        {
+                            dic[color].Add(cloth, 1);
+                        }
+                        else//increase the count
+                        {
+                            dic[color][cloth]++;
+                        }
+                    }
+                }
+            }
+            bool isFound=false;
+            string[] lookingFor=Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            string colorToLookFor=lookingFor[0];
+            string pieceToLookFor=lookingFor[1];
+            foreach (var item in dic)
+            {
+                Console.WriteLine($"{item.Key} clothes:");
+                foreach(var cloth in item.Value)
+                {
+                    if (item.Key == colorToLookFor && cloth.Key == pieceToLookFor)
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value} (found!)");
+                    }
+                    else
+                    {
+                         Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
+                    }
+                   
+                }
+            }
+        }
+    }
+}
