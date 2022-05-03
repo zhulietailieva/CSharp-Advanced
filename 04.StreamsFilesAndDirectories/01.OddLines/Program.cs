35174aec4a7605cbd1e32776ffff30c66c10a36f
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-05-04 00:20:58 +0300

    Added a new exercise to the Files, Streams and Directories solution.

---
+++ 04.StreamsFilesAndDirectories/01.OddLines/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\..\Files\input.txt";
+           // File.CreateText(@"..\..\..\Files\output.txt");
+            string outputFilePath = @"..\..\..\..\Files\output.txt";
+            
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamWriter writer=new StreamWriter(outputFilePath);
+            StreamReader reader=new StreamReader(inputFilePath);
+            using (writer)
+            {
+                using (reader)
+                {
+                    string line=reader.ReadLine();
+                    int lineCount = 0;
+
+                    while (line != null)
+                    {
+                        if (lineCount % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        line= reader.ReadLine();
+                        lineCount++;
+
+                    }
+                }
+            }
+        }
+    }
+}
