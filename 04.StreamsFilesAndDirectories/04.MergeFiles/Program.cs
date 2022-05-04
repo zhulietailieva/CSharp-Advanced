8c581eb2a2514b66db39a02e9273ad6dc1eda548
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-05-04 22:59:47 +0300

    Fixed the error in the exercise that wasn't working.

--- 04.StreamsFilesAndDirectories/04.MergeFiles/Program.cs
+++ 04.StreamsFilesAndDirectories/04.MergeFiles/Program.cs
@@ -13,95 +13,54 @@ namespace _04.MergeFiles
             string firstInputFilePath = @"..\..\..\..\Files\04input1.txt";
             string secondInputFilePath = @"..\..\..\..\Files\04input2.txt";
             string outputFilePath = @"..\..\..\..\Files\04output.txt";
-            MergeTextFiles(firstInputFilePath,secondInputFilePath,outputFilePath);
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
         }
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
         {
             StreamWriter writer = new StreamWriter(outputFilePath);
-          //using (writer)
-          //{
-            StreamReader file1reader=new StreamReader(firstInputFilePath);
-            StreamReader file2reader=new StreamReader(secondInputFilePath);
-            
-            
-            int file1L = file1reader.ReadToEnd().Length;
-            int file2L = file2reader.ReadToEnd().Length;
-            
-            
-
-                Console.WriteLine(file1reader.ReadLine());
-            
-            if (file1L >= file2L)
+            using (writer)
             {
-                    Console.WriteLine("here");
+                StreamReader file1reader = new StreamReader(firstInputFilePath);
+                StreamReader file2reader = new StreamReader(secondInputFilePath);
+
                 using (file1reader)
                 {
                     using (file2reader)
                     {
-                       
-                        string file1Line = file1reader.ReadLine();
-                        string file2Line = file2reader.ReadLine();
-                        while (file1Line != null && file2Line != null)
+
+                        string lineInp1 = file1reader.ReadLine();
+                        string lineInp2 = file2reader.ReadLine();
+
+                        while (lineInp1 != null && lineInp2 != null)
                         {
-                                writer.WriteLine(file1Line);
-                                writer.WriteLine(file2Line);
-                                Console.WriteLine(file1Line);
-                                Console.WriteLine(file2Line);
-                                file1Line = file1reader.ReadLine();
-                                file2Line = file2reader.ReadLine();
+                            writer.WriteLine(lineInp1);
+                            writer.WriteLine(lineInp2);
+                            lineInp1 = file1reader.ReadLine();
+                            lineInp2 = file2reader.ReadLine();
 
                         }
-                    }
-                        string file1LinesLeft = file1reader.ReadLine();
-                    while(file1LinesLeft != null)
+                        if (lineInp1 != null)
                         {
-                            writer.WriteLine(file1LinesLeft);
-                            Console.WriteLine(file1LinesLeft);
-                            file1LinesLeft = file1reader.ReadLine();
-                            
-                        }
-                }
-            }
-            else
-            {
-                    string file1Line = file1reader.ReadLine();
-                    string file2Line = file2reader.ReadLine();
-                    Console.WriteLine("here1");
-                    Console.WriteLine(file1Line + " line 1");
-                    Console.WriteLine(file2Line + " line 2");
-                    using (file2reader)
-                {
-                    using (file1reader)
-                    {
-                            
-                            //string file1Line = file1reader.ReadLine();
-                            //string file2Line = file2reader.ReadLine();
-                            //tuk gurmi te sa null zashto?
-                            Console.WriteLine(file1Line+" line 1");
-                            Console.WriteLine(file2Line+" line 2");
-                            while (file1Line != null && file2Line != null)
+                            while (lineInp1 != null)
                             {
-                                writer.WriteLine(file1Line);
-                                writer.WriteLine(file2Line);
-                                Console.WriteLine(file1Line);
-                                Console.WriteLine(file2Line);
-                                file1Line = file1reader.ReadLine();
-                                file2Line = file2reader.ReadLine();
+                                writer.WriteLine(lineInp1);
+                                lineInp1 = file1reader.ReadLine();
                             }
                         }
-                        string file2LinesLeft = file2reader.ReadLine();
-                        while (file2LinesLeft != null)
+                        if (lineInp2 != null)
                         {
-                            writer.WriteLine(file2LinesLeft);
-                            Console.WriteLine(file2LinesLeft);
-                            file2LinesLeft = file2reader.ReadLine();
-
+                            while (lineInp2 != null)
+                            {
+                                writer.WriteLine(lineInp1);
+                                lineInp2 = file2reader.ReadLine();
+                            }
                         }
                     }
                 }
+                
+
             }
-         // }
-          }
-        
+        }
+    }
     }
 
