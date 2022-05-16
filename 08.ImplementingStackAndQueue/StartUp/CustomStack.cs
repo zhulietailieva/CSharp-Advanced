7735eb0514c3a19ef7a62da148e2df1ffe773574
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-05-16 15:43:27 +0300

    Created customStack<int> class.

---
+++ 08.ImplementingStackAndQueue/StartUp/CustomStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartUp
+{
+    public class CustomStack
+    {
+        private const int InitialCapacity = 4;
+        private int[] array;
+         public CustomStack()
+        {
+            array = new int[InitialCapacity];
+            Count = 0;
+        }
+        public int Count { get;private set; }
+        
+          
+       
+        public void Push(int element)
+        {
+
+            Resize();         
+            array[Count] = element;
+            Count++;
+        }
+        public int Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            Count--;
+            return array[Count];
+        }
+        public int Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return array[Count - 1];
+        }
+        public void ForEach(Action<int> action)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                action(array[i]);
+            }
+        }
+
+        private void Resize()
+        {
+            if (Count==array.Length)
+            {
+                int[] copyArr = new int[array.Length * 2];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    copyArr[i] = array[i];
+                }
+                array = copyArr;
+            }
+           
+        }
+
+    }
+//    void Push(int element) – Adds the given element to the stack
+//int Pop() – Removes the last added element
+//int Peek() – Returns the last element in the stack without removing it
+//void ForEach(Action<int> action) – Goes through each of the elements in the stack
+}
+
--- 08.ImplementingStackAndQueue/StartUp/Test.cs
+++ 08.ImplementingStackAndQueue/StartUp/Test.cs
@@ -6,6 +6,7 @@ namespace StartUp
     {
         static void Main(string[] args)
         {
+            //Testing CustomList structure
             CustomList customList = new CustomList();
             customList.Add(7);
             customList.Add(2);
@@ -13,10 +14,22 @@ namespace StartUp
             Console.WriteLine(customList.RemoveAt(2));
             for (int i = 0; i < customList.Length; i++)
             {
-                Console.WriteLine(customList[i]) ;
+                Console.WriteLine(customList[i]);
             }
             Console.WriteLine(customList.Contains(7));
             Console.WriteLine(customList.Contains(33));
+            //Testing CustomStack structure
+            CustomStack customStack = new CustomStack();
+            customStack.Push(5);
+            customStack.Push(2);
+            Console.WriteLine(customStack.Peek());
+            Console.WriteLine(customStack.Peek());
+            customStack.ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+           customStack.Pop();
+            
          
         }
     }
