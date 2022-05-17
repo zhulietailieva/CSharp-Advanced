0a5f8f829fa21b9472c3d06b570ebcd9076eac0a
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-05-17 16:29:05 +0300

    Added a Generics exercise.

---
+++ 09.Generics/BoxOfT/Box.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace BoxOfT
+{
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    public class Box<T>
+    {
+        Stack<T> elements;
+        public Box()
+        {
+            elements = new Stack<T>();
+
+        }
+        public int Count
+        {
+            get
+            {
+                return this.Count;
+            }
+        }
+        public void Add(T element)
+        {
+            elements.Push(element);
+        }
+        public T Remove()
+        {
+            return elements.Pop();
+        }
+
+    }
+}
