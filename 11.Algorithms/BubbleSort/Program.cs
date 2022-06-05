6b251c00a8a3e283e7a5e87f27afa82d54a7a495
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-06-05 14:58:29 +0300

    Added some Sorting exercises.

---
+++ 11.Algorithms/BubbleSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                int[] arr = { 12,66,3,5,7,9 };
+                int temp;
+                for (int j = 0; j <= arr.Length - 2; j++)
+                {
+                    for (int i = 0; i <= arr.Length - 2; i++)
+                    {
+                        if (arr[i] > arr[i + 1])
+                        {
+                            temp = arr[i + 1];
+                            arr[i + 1] = arr[i];
+                            arr[i] = temp;
+                        }
+                    }
+                }
+               
+                foreach (int p in arr)
+                    Console.Write(p + " ");
+                Console.Read();
+            }
+        
+    }
+}
---
+++ 11.Algorithms/SelectionSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1,5,7,3,2,79,8,5,7,0 };
+            int n = arr.Length;
+            
+            int temp, smallest;
+            for (int i = 0; i < n - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
+            }          
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+
+}
