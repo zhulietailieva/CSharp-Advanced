72d273232e8473d3ab18d3844f060876384b5442
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-06-05 10:15:49 +0300

    Added a new exercise and a Problems description file.

---
+++ 10.IteratorsAndComparators/Exercise/06.CustomComparator/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.CustomComparator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Func<int, int, int> sortArray = (x, y) =>
+              {
+                  if (x % 2 == 0 && y % 2 != 0) return -1;
+                  else if (x % 2 != 0 && y % 2 == 0) return 1;
+                  else if (x > y) return 1;
+                  else if (x < y) return -1;
+                  return 0;
+
+              };
+          Array.Sort(arr, (x,y)=>sortArray(x,y));
+            Console.WriteLine(String.Join(" ",arr));
+        }
+       
+    }
+}
