a5ad604eec73c22678b7031dbd22e4809d99134b
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-06-05 09:47:00 +0300

    Added an IComparable<T> exercise.

---
+++ 10.IteratorsAndComparators/Exercise/05.ComparingObjects/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _05.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public string Name { get { return this.name; } }
+        public int Age { get { return this.age; } }
+        public string Town { get { return this.town; } }
+        public Person(string name,int age,string town)
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+
+        public int CompareTo( Person other)
+        {
+            int res = this.Name.CompareTo(other.Name);
+            if (res == 0)
+            {
+                res = this.Age.CompareTo(other.Age);
+                if (res == 0)
+                {
+                    return this.Town.CompareTo(other.Town);
+                }
+                return res;
+            }
+            return res;
+        }
+    }
+}
---
+++ 10.IteratorsAndComparators/Exercise/05.ComparingObjects/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> list = new List<Person>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] inpData = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(inpData[0], int.Parse(inpData[1]), inpData[2]);
+                list.Add(person);
+            }
+            int n = (int.Parse(Console.ReadLine())) - 1;
+            Person currPerson = list[n];
+            int notEqual = 0;
+            int matches = 0;
+            int totalNum = list.Count;
+            foreach(Person person in list)
+            {
+                if (currPerson.CompareTo(person) == 0)
+                {
+                    matches++;
+                }
+                else
+                {
+                    notEqual++;
+                }
+            }
+            if (matches == 1)
+            {
+                Console.WriteLine("No matches");
+                return;
+            }
+            Console.WriteLine($"{matches} {notEqual} {totalNum}");
+        }
+    }
+}
